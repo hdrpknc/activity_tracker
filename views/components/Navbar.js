a883d8fe482137323598689072bc4a0a0ee025c1
hdrpknc <hdrpknc@users.noreply.github.com>
2019-06-16 16:23:32 +0200

    fix

--- views/components/Navbar.js
+++ views/components/Navbar.js
@@ -2,7 +2,12 @@ let Navbar = {
   render: async () => {
     let view = /*html*/ `
              <nav role="navigation" aria-label="main navigation">
-                    <div id="navbar" aria-expanded="false">
+                    <div id="navbar-brand" class="navbar-brand">
+                        <a class="navbar-item" href="/#/">
+                            activity tracker
+                        </a>
+                    </div>
+                    <div id="navbar-items" aria-expanded="false">
                         <div >
                             <a class="navbar-item" href="/activity_tracker/#/">
                                 Home
