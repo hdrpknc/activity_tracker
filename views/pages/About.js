52b71171910523816f5138df092252a827c28553
hdrpknc <hdrpknc@users.noreply.github.com>
2019-06-16 15:38:42 +0200

    updating about page

--- views/pages/About.js
+++ views/pages/About.js
@@ -3,6 +3,8 @@ let About = {
         let view =  /*html*/`
             <section class="section">
                 <h1> About </h1>
+                <p>This website allows you to monitor your activities of any kind.</p>
+                <p>Data is exclusively stored on your devices.</p>
             </section>
         `
         return view
