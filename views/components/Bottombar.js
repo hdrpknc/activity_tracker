f6669058f00b0889589a460b60c6e5861cffb06c
hdrpknc <hdrpknc@users.noreply.github.com>
2019-06-16 15:22:47 +0200

    test

--- views/components/Bottombar.js
+++ views/components/Bottombar.js
@@ -4,7 +4,7 @@ let Bottombar = {
         <footer class="footer">
             <div class="content has-text-centered">
                 <p>
-                    Footer - ToDo
+                    a hdrpknc production
                 </p>
             </div>
         </footer>
